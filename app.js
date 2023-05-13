f723d2b84cdf9913a69995e29c2171c69dc7f07d
Md.Asif Parvez Sarker <AsifParvezsarker26@gmail.com>
2023-05-13 21:40:52 +0600

    app

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.use(express.json());
 
 app.use('/fakeprofiles',personRoutes);
 
-app.listen(3000,() => {
-    console.log(`Server is running on port 3000`);
+app.listen(3012,() => {
+    console.log(`Server is running on port 3012`);
 })
 
 
